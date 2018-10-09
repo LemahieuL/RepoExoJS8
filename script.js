a9fecc589fc2b2c9b7b3efae1c7da619bfa03f8d
Luc <luc.lemahieu@laposte.net>
2018-10-09 16:43:11 +0200

    meilleur message

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ var regex = /^[0-9]+$/
 submit.addEventListener('click', validation)
 
 function validation(){
-if(regex.test(age.value)){
-  if(age.value >= 18){
-    alert('Vous êtes majeur')
+  if(regex.test(age.value)){
+    if(age.value <110){
+      if(age.value >= 18){
+        alert('Vous êtes majeur')
+      } else {
+        alert('Vous êtes mineur')
+      }
+    } else {
+      alert('Tu es mort')
+    }
   } else {
-    alert('Vous êtes mineur')
+    alert('Donnée incorrecte')
   }
-} else {
-  alert('Donnée incorrecte')
-}
 }
